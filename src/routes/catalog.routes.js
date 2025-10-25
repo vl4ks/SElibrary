ab0263204af419f3fe59e0832be93e1b5c791d8b
Max <fedonkin145@gmail.com>
2025-10-25 11:59:56 +0500

    add catalog endpoints

--- src/routes/catalog.routes.js
+++ src/routes/catalog.routes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const catalogController = require('../controllers/catalog.controller')
 
-router.get('/search', catalogController.search.bind(catalogController))
+router.post('/search', catalogController.search.bind(catalogController))
 router.get('/books/:bookId', catalogController.getBook.bind(catalogController))
 router.get('/authors/:bookId', catalogController.getAuthorsDetails.bind(catalogController))
 
