8fd7f5a7e3d0947054386aa9102b4c638a05c19c
Max <fedonkin145@gmail.com>
2025-10-25 11:43:46 +0500

    fix

--- public/js/auth.logout.js
+++ public/js/auth.logout.js
@@ -1,7 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
     const logoutButton = document.getElementById('logoutButton')
     logoutButton.addEventListener('click', async () => {
-        const response = await fetch('/api/auth/logout')
+        const response = await fetch('/api/auth/logout', {
+            method: 'GET',
+            headers: {'Content-Type': 'application/json'}
+        })
         const result = await response.json()
 
         if (result.redirect) {
